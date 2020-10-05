0f9171ea4964e1f862a7315c725143625b5d7264
Hassam Siddiqui <hassam363@gmail.com>
2020-10-05 14:54:14 +0500

    port defined in config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const port = 4000; // pull it from the config
+const config = require("./config");
 const app = require("./app.js");
+const port = config.get("port");
 
 app.listen(port, () => {
   console.log(`server running on port: ${port}`);
