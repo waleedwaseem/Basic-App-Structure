ba088b1eff11ceadc0bfe6eafc8ae2f1493277c0
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2021-11-19 16:10:44 +0500

    added test boilerplate code for running migrations
    * npm test script updated

---
+++ test/test.js
@@ -0,0 +1,29 @@
+const Umzug = require("umzug");
+const path = require("path");
+const Sequelize = require("sequelize");
+const { sequelize } = require("../models/index.js");
+
+// config Umzug for running migrations
+const umzug = new Umzug({
+  migrations: {
+    // path to migrations files folder
+    path: path.join(process.cwd(), "database", "migrations/"),
+    // inject sequelize's QueryInterface in the migrations
+    params: [sequelize.getQueryInterface(), Sequelize],
+  },
+  // indicates that the migration data should be store in the database
+  // itself through sequelize. The default configuration creates a table
+  // named `SequelizeMeta`.
+  storage: "sequelize",
+  storageOptions: {
+    sequelize,
+  },
+});
+
+// async function migrate() {
+//   return await umzug.up();
+// }
+
+// async function revert() {
+//   return await umzug.down({ to: 0 });
+// }
