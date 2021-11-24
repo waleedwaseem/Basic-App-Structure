b0c4356906e9ca39a5b6e85dbe6ad05a190434c2
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2021-11-24 17:13:04 +0500

    test added: create foo in db

--- test/setup/hook.js
+++ test/setup/hook.js
@@ -1,8 +1,7 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 
-const { migrate, revert } = require("./migration-runner");
-const app = require("../../app");
+const { migrate } = require("./migration-runner");
 
 chai.use(chaiHttp);
 
@@ -14,15 +13,7 @@ exports.mochaHooks = {
   },
   afterAll: async function () {
     // do something before all test once
-    console.log("closing server");
-    requester.close();
-
-    console.log("reverting migrations...");
-    await revert();
+    // console.log("reverting migrations...");
+    // await revert();
   },
 };
-
-console.log("starting server");
-const requester = chai.request(app).keepOpen();
-
-exports.serverRequester = requester;
--- test/utils/index.js
+++ test/utils/index.js
@@ -1,9 +1,8 @@
+const chai = require("chai");
 const expect = require("chai").expect;
 const path = require("path");
 const pathToUtilsIndex = path.join(process.cwd(), "utils", "index.js");
 const isEmailValid = require(pathToUtilsIndex).isEmailValid;
-
-// const { serverRequester } = require("../setup/hook");
 
 describe("test connstants", function () {
   it("isEmailValid should return true", function () {
@@ -12,5 +11,19 @@ describe("test connstants", function () {
 
   it("isEmailValid should return false", function () {
     expect(isEmailValid("abc.def@mail")).to.be.false;
+  });
+});
+
+describe("create Foo route", function () {
+  it("should create a Foo in db", function (done) {
+    chai
+      .request("http://localhost:4000")
+      .post("/api/foos/signup")
+      .send({ firstName: "uzair" })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 });
