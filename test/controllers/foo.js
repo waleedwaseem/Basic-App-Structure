7a73f87cb097c91809262d4a4a06caf7c42471bf
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2021-11-25 14:43:02 +0500

    updated test: create Foo

--- test/controllers/foo.js
+++ test/controllers/foo.js
@@ -1,30 +1,27 @@
 const chai = require("chai");
 const expect = require("chai").expect;
+const { Foo } = require("../../models");
 
 describe("foo Controller", function () {
   describe("create Foo", function () {
-    it("should create a Foo in db", function (done) {
-      chai
+    it("should create a Foo in db", async function () {
+      let myName = "Muhammad Uzair";
+      let response = await chai
         .request("http://localhost:4000")
         .post("/api/foos/signup")
-        .send({ firstName: "me" })
-        .end(function (err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          done();
-        });
+        .send({ firstName: myName });
+      expect(response).to.have.status(200);
+      expect(await Foo.findOne({ where: { firstName: myName } }))
+        .to.have.property("firstName")
+        .equal(myName);
     });
 
-    it("shouldn't create a Foo in db", function (done) {
-      chai
+    it("shouldn't create a Foo in db", async function () {
+      const response = await chai
         .request("http://localhost:4000")
         .post("/api/foos/signup")
-        .send({})
-        .end(function (err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(400);
-          done();
-        });
+        .send({});
+      expect(response).to.have.status(400);
     });
   });
 });
