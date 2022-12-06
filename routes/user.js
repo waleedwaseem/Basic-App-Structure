6cfa1ae4e431b4a9e388edfb0edb69e75b2a050c
Bilal Ahmad <bilal.khattak69@gmail.com>
2022-12-06 03:45:47 -0800

    rename routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
-router.get("/", userController.getUsers);
-router.get("/:user_id", userController.getUser);
+router.get("/", userController.getAll);
+router.get("/:id", userController.getOne);
 router.post("/signup", userController.create);
-router.delete("/:user_id", userController.remove);
-router.put("/:user_id", userController.update);
+router.delete("/:id", userController.archive);
+router.put("/:id", userController.update);
 
 module.exports = router;
